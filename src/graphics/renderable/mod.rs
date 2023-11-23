5fd814d9203ad19c58cf365e7224c99edd67e09c
Michael <mikerule68@gmail.com>
2023-11-22 20:48:08 -0700

    changed instance renderables; added r_instance;

--- src/graphics/renderable/mod.rs
+++ src/graphics/renderable/mod.rs
@@ -1,9 +1,13 @@
 mod r_vertex;
 mod r_index;
+mod r_instance;
+mod r_inst_vertex;
 mod r_inst_index;
 
 pub use self::r_vertex::Vertex;
 pub use self::r_index::Index;
+pub use self::r_instance::Instance;
+pub use self::r_inst_vertex::InstanceVertex;
 pub use self::r_inst_index::InstanceIndex;
 
 pub trait OnDeserialization {
--- src/graphics/renderable/r_inst_vertex.rs
+++ src/graphics/renderable/r_inst_vertex.rs
@@ -3,34 +3,33 @@ use std::io;
 use serde::{Serialize, Deserialize};
 use wgpu::{Device, SurfaceConfiguration};
 
-use super::{Index, Instance, OnDeserialization};
+use super::{OnDeserialization, Vertex, Instance};
 use crate::graphics::{InstanceBuffer, Shader, VertexBuffer, Layout};
 
 #[derive(Serialize, Deserialize)]
-pub struct InstanceIndex {
-    pub r_index: Index,
+pub struct InstanceVertex {
+    pub r_vertex: Vertex,
     pub r_instance: Instance,
 }
 
-impl InstanceIndex {
+impl InstanceVertex {
     pub fn new(
         hash: u64,
         device: &Device,
         config: &SurfaceConfiguration,
         shader: &Shader,
         buffer_list: Vec<VertexBuffer>,
-        index_list: Vec<u16>,
         inst_list: Vec<InstanceBuffer>,
-        buffer_layouts: &mut Vec<wgpu::VertexBufferLayout<'static>>,
+        buffer_layouts: &mut Vec<wgpu::VertexBufferLayout<'static>>
     ) -> Result<Self, io::Error> {
 
         buffer_layouts.insert(0, InstanceBuffer::layout());
 
-        let r_index = Index::new(hash, device, config, shader, buffer_list, index_list, buffer_layouts)?;
+        let r_vertex = Vertex::new(hash, device, config, shader, buffer_list, buffer_layouts)?;
         let r_instance = Instance::new(device, inst_list)?;
 
         Ok(Self {
-            r_index,
+            r_vertex,
             r_instance,
         })
     }
@@ -41,21 +40,20 @@ impl InstanceIndex {
         config: &SurfaceConfiguration,
         shader: &Shader,
         buffer_list: Vec<VertexBuffer>,
-        index_list: Vec<u16>,
         inst_list: Vec<InstanceBuffer>,
         buffer_layouts: &mut Vec<wgpu::VertexBufferLayout<'static>>
     ) -> Result<(), io::Error> {
 
         buffer_layouts.insert(0, InstanceBuffer::layout());
 
-        self.r_index.modify(device, config, shader, buffer_list, index_list, buffer_layouts)?;
+        self.r_vertex.modify(device, config, shader, buffer_list, buffer_layouts)?;
         self.r_instance.modify(device, inst_list)?;
 
         Ok(())
     }
 }
 
-impl OnDeserialization for InstanceIndex {
+impl OnDeserialization for InstanceVertex {
     fn init(
         &mut self, 
         device: &Device,
@@ -63,8 +61,8 @@ impl OnDeserialization for InstanceIndex {
         shader: &Shader,
         buffer_layouts: &mut Vec<wgpu::VertexBufferLayout<'static>>
     ) -> Result<(), std::io::Error> {
-        
-        self.r_index.init(device, config, shader, buffer_layouts)?;
+
+        self.r_vertex.init(device, config, shader, buffer_layouts)?;
         self.r_instance.init(device, config, shader, buffer_layouts)?;
 
         Ok(())
